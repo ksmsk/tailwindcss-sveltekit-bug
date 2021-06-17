5f83cdea4bc7895bed37b9e307df081f1c04fb12
Kubilen Simsek <simsek@kubilen.com>
2021-06-18 01:45:35 +0300

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,2 @@
+<h1 class="bg-yellow-500/90">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import("tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.svelte'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
